b46e088dbd9d8e454038c1ec5da9c2297f471f2e
Alissa Cheng <alissa.cheng@protonmail.ch>
2020-11-18 21:40:25 +0100

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,31 +1,39 @@
-def pearsoncoef(x,y):
+import numpy as np
+
+def pearsoncoef(x, y):
+    """
+    Given data arrays x and y, returns Pearson correlation coefficient (Pearson's r).
+    """
     xm = x.mean(); ym = y.mean()
-    Sxy = ((x-xm)*(y-ym)).sum()
-    Sxx = ((x-xm)*(x-xm)).sum()
-    Syy = ((y-ym)*(y-ym)).sum()
-    r = Sxy/(Sxx**0.5*Syy**0.5)
+    Sxy = ((x-xm) * (y-ym)).sum()
+    Sxx = ((x-xm) * (x-xm)).sum()
+    Syy = ((y-ym) * (y-ym)).sum()
+    r = Sxy / (Sxx**0.5 * Syy**0.5)
     return r
     
-def weightedmoments(x,w):
+def weightedmoments(x, w):
+    """
+    Given data array x and weights array w, returns the weighted moments.
+    """
     gam = w.sum()
     XY = (x*w).sum()
-    mean = XY/gam
-    var = (1/gam)*(w*(x-mean)**2).sum()
+    mean = XY / gam
+    var = (1/gam) * (w*(x-mean)**2).sum()
     std = np.sqrt(var)
-    skew = (1/gam)*(w*(x-mean)**3).sum()/std**3
-    kurt = (1/gam)*(w*(x-mean)**4).sum()/std**4
+    skew = (1/gam) * (w*(x-mean)**3).sum() / std**3
+    kurt = (1/gam) * (w*(x-mean)**4).sum() / std**4
     return mean, var, std, skew, kurt
     
-def denoise(s, n=10):
+def denoise(sig, n=10):
     """
     Denoises 1D signal s with the procedure - 
     apply FFT, filter by taking only n terms, apply inverse FFT.
     Returns denoised signal and complex terms
     (use attributes .real and .imag).
     """
-    C = np.fft.fft(s)
-    N = len(s)
-    start = n; end = N-n
-    C[start:end] = 0.0
-    D = np.fft.ifft(C)
-    return D, C/N
+    transform = np.fft.fft(sig)
+    N = len(sig)
+    end = N-n
+    transform[n:end] = 0.0
+    denoised = np.fft.ifft(transform)
+    return denoised, transform/N
